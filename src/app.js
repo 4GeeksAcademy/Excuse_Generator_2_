9f25b326cbded0e0cf688984649c092ce7a41aae
Alexander Hinostroza Montes <xandereloy99@gmail.com>
2025-05-16 11:57:15 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+    window.generarExcusa = function () {
+        let randomWho = Math.floor(Math.random() * who.length);
+        let randomAction = Math.floor(Math.random() * action.length);
+        let randomWhat = Math.floor(Math.random() * what.length);
+        let randomWhen = Math.floor(Math.random() * when.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+        let excusa = `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+        document.querySelector("#excusa").innerHTML = excusa;
+    };
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    window.cambiarColor = function () {
+        const colores = ['red', 'blue', 'green', 'purple', 'orange'];
+        let color = colores[Math.floor(Math.random() * colores.length)];
+        document.getElementById("excusa").style.color = color;
+    };
+
+    generarExcusa(); 
 };
